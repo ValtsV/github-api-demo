b1280ba8a4461a9e7ddce3abeb8841e60c208959
Valts Vilcans <valts03@inbox.lv>
2021-11-04 10:50:06 +0100

    Añadidos contador de fichero y contador de lineas en ficheros

--- src/main/java/com/vala/github/api/demo/GithubConnect.java
+++ src/main/java/com/vala/github/api/demo/GithubConnect.java
@@ -6,10 +6,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import javax.sound.midi.Soundbank;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 @Component
 public class GithubConnect {
@@ -22,36 +29,69 @@ public class GithubConnect {
     public void getRepos() {
         try {
 
+//            Crea conexion autorizada a GitHub y pilla una lista de los repos
             String oauth = env.getProperty("oauth");
 			GitHub github = new GitHubBuilder().withOAuthToken(oauth).build();
             GHUser user = github.getUser("Open-Bootcamp");
-           user.getRepositories();
+            user.getRepositories();
+
             for (Map.Entry<String, org.kohsuke.github.GHRepository> entry : user.getRepositories().entrySet()) {
                 System.out.println(entry.getKey() + "/" + entry.getValue().getName());
-//                GHRepository lol = github.getRepository(entry.getKey());
-//                toString(lol.listCommits());
+
+
                 if (entry.getValue().getSize() != 0) {
 
                     PagedIterable<GHCommit> commits = entry.getValue().listCommits();
 
-                    Iterator<GHCommit> iterator = commits.iterator();
 
+                    Iterator<GHCommit> iterator = commits.iterator();
+                    String sha = "";
                     while (iterator.hasNext()) {
                         GHCommit commit = iterator.next();
-//                        Iterator<GHCommit.File> iter = commit.getFiles().iterator();
-//                        while (iterator.hasNext()) {
-//                            GHCommit.File lol = iter.next();
-//                            System.out.println(lol.getFileName());
-//                            System.out.println(lol.getRawUrl().getFile().lines());
-//                        }
 
-//                    if (commit != null) {
+
                         System.out.println("Commit: " + commit.getSHA1() + ", info: " + commit.getCommitShortInfo().getMessage());
-
-//                    } else {
-//                        System.out.println("No commits yet");
-//                    }
+                        sha = commit.getSHA1();
                     }
+
+                    HashMap<String, Integer> fileCount = new HashMap<>();
+                    var x = entry.getValue().getCommit(sha).getFiles();
+                    Iterator<GHCommit.File> iter = x.iterator();
+                    while (iter.hasNext()) {
+                        GHCommit.File lol = iter.next();
+                        String fileName = lol.getFileName();
+                        String fileExtension = fileName.substring(fileName.lastIndexOf(".")+1);
+                        if (!fileCount.containsKey(fileExtension)) {
+                            fileCount.put(fileExtension, 1);
+                        } else {
+                            fileCount.put(fileExtension, fileCount.get(fileExtension) + 1);
+                        }
+
+
+                        System.out.println(lol.getRawUrl());
+                        URL url = lol.getRawUrl();
+                        StringBuilder sb = new StringBuilder();
+                        String line;
+
+                        int count = 0;
+                        InputStream in = url.openStream();
+                        try {
+
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                            while ((line = reader.readLine()) != null) {
+                                count++;
+                            }
+                        } finally {
+                            in.close();
+                        }
+
+                        System.out.println(count);
+                    }
+
+//                    System.out.println(fileCount.get("java"));
+                    fileCount.entrySet().forEach(file -> {
+                        System.out.println(file.getKey() + " " + file.getValue());
+                    });
 
                 } else {
                     System.out.println("No commits yet");
